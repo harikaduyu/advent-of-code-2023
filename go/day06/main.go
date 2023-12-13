9d26fb43a12a454da550c6d9138946f7dc09dcd9
Harika <harika.duyu@gmail.com>
2023-12-13 08:51:21 +0100

    day06

---
+++ go/day06/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/harikaduyu/advent-of-code-2023/go/utils"
+)
+
+func getNumWaysCouldWin(time int, distance int) int {
+	numWaysCouldWin := 0
+
+	for i := time / 2; i >= 0; i-- {
+
+		travel := i * (time - i)
+
+		if travel > distance {
+			numWaysCouldWin++
+		} else {
+			numWaysCouldWin *= 2
+			if time%2 == 0 {
+				numWaysCouldWin--
+			}
+			break
+		}
+
+	}
+
+	return numWaysCouldWin
+
+}
+
+func parse(input string) ([]string, []string) {
+	splitted := strings.Split(input, "\n")
+	times := strings.Fields(strings.TrimSpace(strings.Split(splitted[0], ":")[1]))
+	distances := strings.Fields(strings.TrimSpace(strings.Split(splitted[1], ":")[1]))
+	return times, distances
+}
+func part1(times []string, distances []string) int {
+	marginOfError := 1
+	for i, ts := range times {
+		time, _ := strconv.Atoi(ts)
+		distance, _ := strconv.Atoi(distances[i])
+		numWaysCouldWin := getNumWaysCouldWin(time, distance)
+
+		marginOfError *= numWaysCouldWin
+	}
+	return marginOfError
+}
+
+func part2(times []string, distances []string) int {
+	timeStr := strings.Join(times, "")
+	distanceStr := strings.Join(distances, "")
+
+	time, _ := strconv.Atoi(timeStr)
+	distance, _ := strconv.Atoi(distanceStr)
+	return getNumWaysCouldWin(time, distance)
+}
+
+func main() {
+
+	input := utils.ReadInput()
+	times, distances := parse(input)
+	startTime := time.Now()
+
+	res1 := part1(times, distances)
+	endTime := time.Now()
+	fmt.Println("Part1:", res1)
+	fmt.Printf("Part1 took %v\n", endTime.Sub(startTime))
+	// 625ns
+
+	startTime = time.Now()
+	res2 := part2(times, distances)
+	endTime = time.Now()
+	fmt.Println("Part2:", res2)
+	fmt.Printf("Part2 took %v\n", endTime.Sub(startTime))
+	// 19.034ms
+}
---
+++ go/day06/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/harikaduyu/advent-of-code-2023/go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test1(t *testing.T) {
+
+	input := utils.ReadExampleInput(1)
+	times, distances := parse(input)
+
+	res1 := part1(times, distances)
+	assert.Equal(t, 288, res1)
+
+}
+
+func Test2(t *testing.T) {
+
+	input := utils.ReadExampleInput(1)
+	times, distances := parse(input)
+	res2 := part2(times, distances)
+	assert.Equal(t, 71503, res2)
+
+}
